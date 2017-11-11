7e67dd8b5d49285ed5f5c2da6195e292449eebb1
Casey Poon <caseycichi>
2017-11-11 02:26:19 -0800

    add

---
+++ keys.js
@@ -0,0 +1,23 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '87KKG1mqrnvjXRGUPcSfkGnNG',
+  consumer_secret: 's4By94cL66v4lrv5SX0MQFbrnE2O2vyJvLHRT0LpkzPBGPKXKV',
+  access_token_key: '926355187610009600-fD5ykxdJov4JN51dMtoHsLOI2FbO3lX',
+  access_token_secret: 'DlHyQknTaUkLt6P4l6XpsRuMYgcnFmmFPBxMQ8Xpq0Cmt',
+}
+
+module.exports = twitterKeys;
+
+// var spotifyKeys = {
+// 	client id: 'fa8bf63d454446bd8fa03d8226c0ea7b',
+// 	client secret: '4772a6d986a840e68a61736fcecc9810',
+// }
+
+// module.exports = spotifyKeys;
+
+var OMBDkeys = {
+	consumer_key: '40e9cece',
+}
+
+module.exports = OMBDkeys;
